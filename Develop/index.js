e7e35d549bb4aff39350a3595ef2974cfa73ca43
Orlando <orlandozepeda20@gmail.com>
2022-10-20 23:51:53 -0700

    added some questions to the index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateREADME = require('./utils/generateREADME')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Write a description of your project.",
+        name: "description",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
