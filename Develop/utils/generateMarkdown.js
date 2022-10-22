d5109bf1004d1b1b3e3c29f42dfa1040eef1aa36
Orlando <orlandozepeda20@gmail.com>
2022-10-21 23:13:28 -0700

    finshed the generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,16 +11,66 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://choosealicense.com/licenses/mit/"
+  }
+  if (license === "Apache") {
+    return "https://www.apache.org/licenses/LICENSE-2.0"
+  }
+  if (license === "Mozilla") {
+    return "https://www.mozilla.org/en-US/MPL/2.0/"
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ""
+  }
+  else {
+    return `## License
+    This project is using the ${license} license. click on the link for more info.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.licenses)}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#Contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Have any further questions?
+  Email: ${data.email}
+  Github: https://github.com/${data.github}
 `;
 }
 
