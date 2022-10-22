c7379c6c65c6000373c1c01b21c03626ce6d0b65
Orlando <orlandozepeda20@gmail.com>
2022-10-21 22:14:52 -0700

    added to generate markdown and index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateREADME = require('./utils/generateREADME')
+const generateREADME = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,6 +9,15 @@ const questions = [
         type: "input",
         message: "What is your github username?",
         name: "username",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            }
+            else {
+                console.log("Enter your github username!")
+                return false;
+            }
+        }
     },
     {
         type: "input",
@@ -24,14 +33,28 @@ const questions = [
         type: "input",
         message: "Write a description of your project.",
         name: "description",
+    },
+    {
+        type: "input",
+        message: "Describe the steps that are need to install your project if it applies.",
+        name: "installation",
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile("./utils/generated-README.md", data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Congrats! Your README has now been generated!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
